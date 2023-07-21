acf3bcd724bd5351cd28fa0e2ada637567df2270
Martins Tararujs <martins@Martinss-MacBook-Pro-2.local>
2023-07-21 23:54:55 +0300

    Quick fox for envs

--- features/support/environment.rb
+++ features/support/environment.rb
@@ -1,10 +1,11 @@
 class Environment
   def get_parameters(env_name)
-    env_name = "dev"
+    # env_name = "dev"
+    env_name = ENV['PLATFORM'] || 'dev'
     flunk("Test environment not specified. Set PLATFORM env. variable before running tests.") if env_name.nil? or env_name.empty?
     env_name = env_name.upcase
     parameters = {}
-    valid_envs = %w(DEV PRD)
+    valid_envs = %w(DEV STG PRD)
     unless valid_envs.include? env_name
       flunk("TEST SCRIPT ERROR: Wrong environment name: #{env_name}. Supported environments: #{valid_envs}")
     end
@@ -12,8 +13,10 @@ class Environment
     case env_name
     when 'DEV'
       parameters[:api_host] = "http://127.0.0.1:9000"
+    when 'STG'
+      parameters[:api_host] = "http://127.0.0.1:9001"
     when 'PRD'
-      parameters[:api_host] = "http://127.0.0.1:9000"
+      parameters[:api_host] = "http://127.0.0.1:9002"
     else
       #   nothing
     end
