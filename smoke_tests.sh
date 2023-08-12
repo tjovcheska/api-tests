7c6a5e73f2fa081147c0528f7c25fdcced6e62e2
merins <teodora.jovcheska@testdevlab.com>
2023-08-12 11:04:46 +0200

    Changes.

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # cucumber --format html --out test-outputs/report-$(date).html
+# New comment
 while true
 do
 	cucumber --format progress -c --format html --out test-output/report_$(date +%T).html
